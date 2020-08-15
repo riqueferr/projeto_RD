2f2d3bf3b19209ea1f0fe59712455db112286e17
Paulo Oliveira <paulo.oliv03@gmail.com>
2020-08-15 18:38:43 -0300

    Produto filial estoque implementado

---
+++ src/main/java/br/com/pi/projeto_RD/model/dto/ProdutoFilialEstoqueDTO.java
@@ -0,0 +1,18 @@
+package br.com.pi.projeto_RD.model.dto;
+
+import br.com.pi.projeto_RD.model.entity.ProdutoEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ProdutoFilialEstoqueDTO {
+    private Integer fk_filial;
+    private ProdutoEntity fk_produto;
+    private Integer qt_estoque;
+    private Integer qt_empenho;
+    private Integer qt_base;
+
+}
