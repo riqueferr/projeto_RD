a438eb50b8d31fa42459d295cdf8075d0223144c
Pedro Araújo <peu.araujoo@hotmail.com>
2020-08-19 19:02:49 -0300

    Ajustes cadastroDeFornecedor

---
+++ ProjetoAngular/ProjetoPI/src/app/abastecimento/cadastro-de-fornecedor/TesteData.ts
@@ -0,0 +1,5 @@
+export class TesteData {
+    constructor(
+        public name: String
+    ) {}
+  }
--- ProjetoAngular/ProjetoPI/src/app/app.component.ts
+++ ProjetoAngular/ProjetoPI/src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
+import { AppData } from './AppData';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl:'./app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dashboard';
-  
+  title = 'app';
+  data = new AppData('');
 
 }
 
+// app.component.ts
+
+// import { Component } from '@angular/core';
+// import {NgForm} from '@angular/forms';
+// @Component({
+//   selector: 'app-root',
+//   template: `
+//     <form #it="ngForm" (ngSubmit)="onSubmit(it)" novalidate>
+//       <input name="first" ngModel required #first="ngModel">
+//       <input name="last" ngModel required #last="ngModel">
+//       <button>Submit</button>
+//     </form>
+//     <p>First name value: {{ first.value }}</p>
+//     <p>First name valid: {{ first.valid }}</p>
+//     <p>last name value: {{ last.value }}</p>
+//     <p>last name valid: {{ last.valid }}</p>
+//     <p>Form value: {{ it.value | json }}</p>
+//     <p>Form valid: {{ it.valid }}</p>
+//   `,
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   onSubmit(it: NgForm) {
+//     console.log(it.value);  // { first: '', last: '' }
+//     console.log(it.valid);  // false
+//   }
+// }
