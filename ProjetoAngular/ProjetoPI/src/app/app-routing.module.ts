8047d031af67f07f1bf2cbb48d8ee4a981c37818
Pedro Araújo <peu.araujoo@hotmail.com>
2020-08-19 01:58:07 -0300

    Migração cadastroDeProduto  p/ Angular

--- ProjetoAngular/ProjetoPI/src/app/app-routing.module.ts
+++ ProjetoAngular/ProjetoPI/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CadastroDeFornecedorComponent } from './abastecimento/cadastro-de-fornecedor/cadastro-de-fornecedor.component';
+import { CadastroDeProdutoComponent } from './abastecimento/cadastro-de-produto/cadastro-de-produto.component';
 
 export const routes: Routes = [
     {
@@ -9,7 +10,9 @@ export const routes: Routes = [
         component: HomeComponent
     },
     {path: 'cadastroDeFornecedor', 
-    component: CadastroDeFornecedorComponent}
+    component: CadastroDeFornecedorComponent},
+    {path:'cadastroDeProduto',
+    component: CadastroDeProdutoComponent}
 ];
 
 @NgModule({
