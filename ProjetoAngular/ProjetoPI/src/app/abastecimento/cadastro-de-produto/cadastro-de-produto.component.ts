8a882b3245d5a9d4fe0d7bbd79e9d979e07a8379
PedrooBelchior <Always.X26@gmail.com>
2020-08-26 12:54:24 -0300

    Ajustes Tela Produto e Fornecedor

--- ProjetoAngular/ProjetoPI/src/app/abastecimento/cadastro-de-produto/cadastro-de-produto.component.ts
+++ ProjetoAngular/ProjetoPI/src/app/abastecimento/cadastro-de-produto/cadastro-de-produto.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-cadastro-de-produto',
   templateUrl: './cadastro-de-produto.component.html',
@@ -10,6 +10,10 @@ export class CadastroDeProdutoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSubmit(it: NgForm) {
+    console.log(it.value);  // { first: '', last: '' }
+    console.log(it.valid);  // false
   }
 
 }
