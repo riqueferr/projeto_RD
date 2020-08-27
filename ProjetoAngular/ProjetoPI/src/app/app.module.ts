6e2069397fa5d2355200f63a1e26c83ed349348c
PedrooBelchior <Always.X26@gmail.com>
2020-08-27 00:04:10 -0300

    Implementação e Consumo de API CEP + AutoComplete

--- ProjetoAngular/ProjetoPI/src/app/app.module.ts
+++ ProjetoAngular/ProjetoPI/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
