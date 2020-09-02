a6529f74ca4b251234af0be115875a3eaf63c3a8
PedrooBelchior <Always.X26@gmail.com>
2020-09-02 15:44:15 -0300

    Implementação sistema básico de login v1.0

--- ProjetoAngular/ProjetoPI/src/app/app.component.ts
+++ ProjetoAngular/ProjetoPI/src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { AppData } from './AppData';
+import { AuthService } from './login/auth.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl:'./app.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
   data = new AppData('');
+
+  mostrarMenu = false;
+
+  constructor(private authService: AuthService) {
+
+  }
+  ngOnInit() {
+    this.authService.mostrarMenuEmitter.subscribe(
+      mostrar => this.mostrarMenu = mostrar
+  );
+  }
 
 }
 
