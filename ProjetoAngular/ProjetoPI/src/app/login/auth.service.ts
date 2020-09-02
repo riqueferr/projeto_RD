f7f928c5ed61ad0022936c0b1563e79162adbcff
PedrooBelchior <Always.X26@gmail.com>
2020-09-02 12:09:44 -0300

    inisicando sistema de login

---
+++ ProjetoAngular/ProjetoPI/src/app/login/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Usuario } from './usuario';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  public usuarioAutenticado = false;
+
+  mostrarMenuEmitter = new EventEmitter<boolean>();
+  constructor(private router: Router) { }
+
+  // tslint:disable-next-line: typedef
+  fazerLogin(usuario: Usuario) {
+
+    // tslint:disable-next-line: triple-equals
+    if (usuario.nome == '166534' &&
+      // tslint:disable-next-line: triple-equals
+      usuario.senha == '123456') {
+
+      this.usuarioAutenticado = true;
+      this.mostrarMenuEmitter.emit(true);
+      this.router.navigate(['/']);
+
+    } else {
+      this.usuarioAutenticado = false;
+    }
+  }
+}
