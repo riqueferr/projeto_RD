9968ded7b3cc850000b8d45aac9fea24ad0bb9d8
PedrooBelchior <Always.X26@gmail.com>
2020-09-03 11:53:22 -0300

    Inserção LoginController

--- src/main/java/br/com/pi/projeto_RD/model/dto/OperadorDTO.java
+++ src/main/java/br/com/pi/projeto_RD/model/dto/OperadorDTO.java
@@ -1,26 +1,25 @@
 package br.com.pi.projeto_RD.model.dto;
 
-
 import br.com.pi.projeto_RD.model.entity.PerfilEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.math.BigInteger;
 import java.util.List;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 public class OperadorDTO {
 
-
-    private Long idOperador;
+    private BigInteger idOperador;
     private String nmOperador;
     private String nrCpf;
-    private Long nrMatricula;
+    private BigInteger nrMatricula;
     private String dsCargo;
-    private Long cdFilial;
+    private BigInteger cdFilial;
     private String pwOperador;
 
     private List<PerfilDTO> perfil;
