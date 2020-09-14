9c2f3a708b5ad12ef6c730368371a231ae283028
Vitor Rodrigues <vitor.rds98@gmail.com>
2020-09-14 14:00:08 -0300

    Performance Dashboard, ajustes transferencia

---
+++ ProjetoAngular/ProjetoPI/src/app/financeiro/cupom-de-venda/listar-filial-df/listar-filial-df.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-filial-df',
+  templateUrl: './listar-filial-df.component.html',
+  styleUrls: ['./listar-filial-df.component.css']
+})
+export class ListarFilialDfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
