a0153ae88d714c34824a098a62b3ea059bbe2576
hersnold <67403434+hersnold@users.noreply.github.com>
2020-09-14 16:28:59 -0300

    Produto API perfomance

--- src/main/java/br/com/pi/projeto_RD/service/RelatorioProdutoService.java
+++ src/main/java/br/com/pi/projeto_RD/service/RelatorioProdutoService.java
@@ -3,6 +3,8 @@ package br.com.pi.projeto_RD.service;
 import br.com.pi.projeto_RD.model.dto.*;
 import br.com.pi.projeto_RD.model.entity.OperacaoEntity;
 import br.com.pi.projeto_RD.model.entity.ProdutoEntity;
+import br.com.pi.projeto_RD.model.entity.StatusProdutoEntity;
+import br.com.pi.projeto_RD.model.entity.TipoProdutoEntity;
 import br.com.pi.projeto_RD.repository.ProdutoRepository;
 import br.com.pi.projeto_RD.service.bo.RelatorioProdutoBO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,15 +31,59 @@ public class RelatorioProdutoService {
     private EntityManager manager;
 
     public List<RelatorioProdutoDTO> listarTodas() {
-        List<ProdutoEntity> listEntity = repository.findAll();
-        List<RelatorioProdutoDTO> listDTO = new ArrayList<>();
+//        List<ProdutoEntity> listEntity = repository.findAll();
+//        List<RelatorioProdutoDTO> listDTO = new ArrayList<>();
+//
+//        for (ProdutoEntity entity : repository.findAll()) {
+//            RelatorioProdutoDTO dto = bo.parseToDTO(entity);
+//            listDTO.add(dto);
+//        }
+//
+//        return listDTO;
 
-        for (ProdutoEntity entity : repository.findAll()) {
-            RelatorioProdutoDTO dto = bo.parseToDTO(entity);
-            listDTO.add(dto);
+        Map<Integer, RelatorioProdutoDTO> map = new HashMap<>();
+
+        Query query = manager.createNativeQuery("SELECT DISTINCT P.CD_PRODUTO, P.NM_FANTASIA, TP.DS_TIPO_PRODUTO, " +
+                "S.DS_STATUS_PRODUTO, FO.NM_RAZAO_SOCIAL FROM TB_PRODUTO  P " +
+                "LEFT OUTER JOIN TB_FORNECEDOR_PRODUTO F ON P.CD_PRODUTO = F.CD_PRODUTO " +
+                "LEFT OUTER JOIN TB_FORNECEDOR FO ON FO.CD_FORNECEDOR = F.CD_FORNECEDOR " +
+                "LEFT OUTER JOIN TB_TIPO_PRODUTO TP ON P.ID_TIPO_PRODUTO = TP.ID_TIPO_PRODUTO " +
+                "LEFT OUTER JOIN TB_STATUS_PRODUTO S ON P.ID_STATUS_PRODUTO = S.ID_STATUS_PRODUTO");
+
+        List<Object[]> listentity = query.getResultList();
+        for(Object [] produto : listentity ){
+            Integer codigo = ((BigInteger) produto [0]).intValue();
+            RelatorioProdutoDTO dto = null;
+            ProdutoEntity entity = new ProdutoEntity();
+            if (!map.containsKey(codigo)){
+                dto = new RelatorioProdutoDTO();
+
+                //Produto
+                dto.setCdProduto((BigInteger) produto[0]);
+                dto.setNmProduto((String) produto[1]);
+                dto.setStatusProduto((String) produto[2]);
+                dto.setTipoProduto((String) produto[3]);
+
+
+                //Fornecedor
+                RelatorioProdutoFornecedorDTO f = new RelatorioProdutoFornecedorDTO();
+//                f.setCd_fornecedor((BigInteger) produto[6]);
+                f.setNm_razao_social((String) produto[4]);
+
+
+                if(dto.getFornecedor() == null)
+                    dto.setFornecedor(new ArrayList<>());
+                dto.getFornecedor().add(f);
+            }else{  dto = map.get(codigo);
+                RelatorioProdutoFornecedorDTO fDTO = new RelatorioProdutoFornecedorDTO();
+                fDTO.setNm_razao_social((String) produto[4]);
+                dto.getFornecedor().add(fDTO);
+            }
+            map.put(dto.getCdProduto().intValue(), dto);
+
+
         }
-
-        return listDTO;
+        return map.values().stream().collect(Collectors.toList());
     }
 
 //    public List<RelatorioProdutoDTO> buscarTodos() {
