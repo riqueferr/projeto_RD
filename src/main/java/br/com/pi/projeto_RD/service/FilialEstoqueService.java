f1deec77174b548a67a6ce7b65106b9d788a7e24
riqueferr <ihenriqueferr@gmail.com>
2020-09-14 15:41:36 -0300

    Mexendo apenas no necessário

--- src/main/java/br/com/pi/projeto_RD/service/FilialEstoqueService.java
+++ src/main/java/br/com/pi/projeto_RD/service/FilialEstoqueService.java
@@ -1,7 +1,11 @@
 package br.com.pi.projeto_RD.service;
 
+import br.com.pi.projeto_RD.model.dto.DFEntradaDTO;
+import br.com.pi.projeto_RD.model.dto.ItensDfDTO;
 import br.com.pi.projeto_RD.model.dto.ProdutoDto;
 import br.com.pi.projeto_RD.model.dto.ProdutoFilialEstoqueDTO;
+import br.com.pi.projeto_RD.model.entity.FilialEntity;
+import br.com.pi.projeto_RD.model.entity.OperacaoEntity;
 import br.com.pi.projeto_RD.model.entity.ProdutoEntity;
 import br.com.pi.projeto_RD.model.entity.ProdutoFilialEstoqueEntity;
 import br.com.pi.projeto_RD.repository.ProdutoFilialEstoqueRepository;
@@ -12,11 +16,9 @@ import org.springframework.stereotype.Service;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,19 +31,64 @@ public class FilialEstoqueService {
     private ProdutoFilialEstoqueRepository repository;
 
 
+
     @PersistenceContext
     private EntityManager manager;
 
+//    public List<ProdutoFilialEstoqueDTO> buscarTodos() {
+//        List<ProdutoFilialEstoqueEntity> pfEntity = repository.findAll();
+//        List<ProdutoFilialEstoqueDTO> filialDTO = new ArrayList<>();
+//
+//
+//        for (ProdutoFilialEstoqueEntity entity : pfEntity) {
+//            ProdutoFilialEstoqueDTO dto = pfBO.parseToDTO(entity);
+//            filialDTO.add(dto);
+//        }
+//        return filialDTO;
+//    }
+
     public List<ProdutoFilialEstoqueDTO> buscarTodos() {
-        List<ProdutoFilialEstoqueEntity> pfEntity = repository.findAll();
-        List<ProdutoFilialEstoqueDTO> filialDTO = new ArrayList<>();
+        Map<Integer, ProdutoFilialEstoqueDTO> map = new HashMap<>();
+
+        Query query = manager.createNativeQuery("SELECT PE.CD_ESTOQUE, FI.CD_FILIAL, FI.NM_FILIAL, PR.CD_PRODUTO, " +
+                "PR.NM_FANTASIA, SC.DS_SUB_CATEGORIA, PE.QT_ESTOQUE, PE.QT_BASE " +
+                "FROM TB_PRODUTO_FILIAL_ESTOQUE PE " +
+                "LEFT OUTER JOIN TB_PRODUTO PR ON PR.CD_PRODUTO = PE.CD_PRODUTO " +
+                "LEFT OUTER JOIN TB_SUB_CATEGORIA_PRODUTO SC ON SC.ID_SUB_CATEGORIA = PR.ID_SUB_CATEGORIA " +
+                "LEFT OUTER JOIN TB_FILIAL FI ON FI.CD_FILIAL = PE.CD_FILIAL " +
+                "ORDER BY PR.CD_PRODUTO");
+
+        List<Object []> listEntity = query.getResultList();
+        for(Object [] produto : listEntity){
+            Integer codigo = ((BigInteger) produto [0]).intValue();
+            ProdutoFilialEstoqueDTO dto = null;
+            if(!map.containsKey(codigo)){
+                dto = new ProdutoFilialEstoqueDTO();
+                dto.setCdEstoque((BigInteger) produto[0]);
+
+                //FILIAL
+                dto.setCdFilial((BigInteger) produto[1]);
+                dto.setNmFilial((String) produto[2]);
 
 
-        for (ProdutoFilialEstoqueEntity entity : pfEntity) {
-            ProdutoFilialEstoqueDTO dto = pfBO.parseToDTO(entity);
-            filialDTO.add(dto);
+                //PRODUTO
+                dto.setCodProduto((BigInteger) produto[3]);
+                dto.setNmProduto((String) produto[4]);
+
+                //SUB CATEGORIA
+                dto.setSubCategoria((String) produto[5]);
+
+                //QTDE ESTOQUE
+                dto.setQt_estoque((Integer) produto[6]);
+
+                //QTDE BASE
+                dto.setQt_base((Integer) produto[7]);
+
+
+           }
+            map.put(dto.getCdEstoque().intValue(), dto);
         }
-        return filialDTO;
+        return map.values().stream().collect(Collectors.toList());
     }
 
     public ProdutoFilialEstoqueDTO buscarPorId(BigInteger codigo) {
@@ -58,14 +105,58 @@ public class FilialEstoqueService {
         return filialDTO;
     }
 
-    public List<ProdutoFilialEstoqueDTO> buscarNmFilial(String nmFilial) {
-        List<ProdutoFilialEstoqueEntity> pfEntity = repository.findByFilialNmFilialContaining(nmFilial);
-        List<ProdutoFilialEstoqueDTO> filialDTO = new ArrayList<>();
-        for (ProdutoFilialEstoqueEntity entity : pfEntity) {
-            ProdutoFilialEstoqueDTO dto = pfBO.parseToDTO(entity);
-            filialDTO.add(dto);
+//    public List<ProdutoFilialEstoqueDTO> buscarNmFilial(String nmFilial) {
+//        List<ProdutoFilialEstoqueEntity> pfEntity = repository.findByFilialNmFilialContaining(nmFilial);
+//        List<ProdutoFilialEstoqueDTO> filialDTO = new ArrayList<>();
+//        for (ProdutoFilialEstoqueEntity entity : pfEntity) {
+//            ProdutoFilialEstoqueDTO dto = pfBO.parseToDTO(entity);
+//            filialDTO.add(dto);
+//        }
+//        return filialDTO;
+//    }
+
+    public List<ProdutoFilialEstoqueDTO> buscarNmFilial(String NM_FILIAL) {
+        Map<Integer, ProdutoFilialEstoqueDTO> map = new HashMap<>();
+
+        Query query = manager.createNativeQuery("SELECT PE.CD_ESTOQUE, FI.CD_FILIAL, FI.NM_FILIAL, PR.CD_PRODUTO, " +
+                "PR.NM_FANTASIA, SC.DS_SUB_CATEGORIA, PE.QT_ESTOQUE, PE.QT_BASE " +
+                "FROM TB_PRODUTO_FILIAL_ESTOQUE PE " +
+                "LEFT OUTER JOIN TB_PRODUTO PR ON PR.CD_PRODUTO = PE.CD_PRODUTO " +
+                "LEFT OUTER JOIN TB_SUB_CATEGORIA_PRODUTO SC ON SC.ID_SUB_CATEGORIA = PR.ID_SUB_CATEGORIA " +
+                "LEFT OUTER JOIN TB_FILIAL FI ON FI.CD_FILIAL = PE.CD_FILIAL " +
+                "WHERE FI.NM_FILIAL LIKE'%"+ NM_FILIAL +"%'");
+
+        List<Object []> listEntity = query.getResultList();
+        for(Object [] produto : listEntity){
+            Integer codigo = ((BigInteger) produto [0]).intValue();
+            ProdutoFilialEstoqueDTO dto = null;
+            if(!map.containsKey(codigo)){
+                dto = new ProdutoFilialEstoqueDTO();
+                dto.setCdEstoque((BigInteger) produto[0]);
+
+                //FILIAL
+                dto.setCdFilial((BigInteger) produto[1]);
+                dto.setNmFilial((String) produto[2]);
+
+
+                //PRODUTO
+                dto.setCodProduto((BigInteger) produto[3]);
+                dto.setNmProduto((String) produto[4]);
+
+                //SUB CATEGORIA
+                dto.setSubCategoria((String) produto[5]);
+
+                //QTDE ESTOQUE
+                dto.setQt_estoque((Integer) produto[6]);
+
+                //QTDE BASE
+                dto.setQt_base((Integer) produto[7]);
+
+
+            }
+            map.put(dto.getCdEstoque().intValue(), dto);
         }
-        return filialDTO;
+        return map.values().stream().collect(Collectors.toList());
     }
 
     public ProdutoFilialEstoqueEntity inserir(ProdutoFilialEstoqueDTO dto) throws Exception {
